55045bd53b9fd3dc2a621324f09e803a1ebc8467
Igor Cavalera <igorcavalera455@gmail.com>
2021-04-26 22:53:23 -0300

    armazenando dados no LocalStorage

---
+++ ceep2/components/criaTarefa.js
@@ -0,0 +1,67 @@
+import BotaoConclui from "./concluiTarefa.js";
+import BotaoDeleta from "./deletaTarefa.js";
+
+//JAVASCRIPT IIFE - Função de Execução Imediata
+//(() => { escopo global }) e "()" <- no final para ser executada
+
+export const handleNovoItem = (evento) => {
+    evento.preventDefault();
+
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+
+    const calendario = document.querySelector('[data-form-date]');
+
+    //Lembrete desfazer esse "moment"
+    const data = moment(calendario.value);
+
+    const dataFormatada = data.format('DD/MM/YY');
+
+    const dados = {
+        valor,
+        dataFormatada
+    }
+
+    const tarefasAtualizadas = [...tarefas, dados];
+
+    const criaTarefa = Tarefa(dados);
+
+    //tarefas.push(dados);
+
+    lista.appendChild(criaTarefa);
+
+    //setItem = 2 valores chave e valor
+    //JSON.stringify() = serve como tradutor de objeto para string
+    /* SessionStorage - armazena os dados enquanto a aplicação estiver sendo
+    utilizada, porém quando se fecha o navegador ou apenas a aba os dados serão
+    perdidos. */
+    //sessionStorage.setItem("tarefas", JSON.stringify(dados));
+    localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas));
+
+    input.value = " ";
+
+}
+
+export const Tarefa = ({valor, dataFormatada}) => {
+//preventDefault() - evita que "recarregue a pagina quando clicar"
+//ele anula o comportamento padrão de um evento
+//evento.preventDefault();
+
+    const tarefa = document.createElement('li');
+    //Classe de css não POO
+    tarefa.classList.add('task');
+
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+
+    return tarefa;
+
+}
+
+//COLOCAR () NO FINAL DA IIFE PARA MOSTRAR QUE ESTÁ SENDO EXECUTADA
